128c8c1a7ecf5bdfd88e53e35d9166592053c3b0
Yuwei Tang <yuweitang98@yahoo.ca>
2018-07-01 18:57:40 -0400

    version 1

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var express = require("express");
 var website = express();
 
+website.use(express.static('assets'));
 
 website.get("/", function(req, res) {
-res.send("A couple more days please, thanks.");
+   res.sendFile(__dirname + '/index.html');
    console.log("Home");
 });
 
+website.get("/index", function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+    console.log("Home again");
+});
 
-website.listen(80,function(){
+website.get("/projects", function (req, res) {
+    res.sendFile(__dirname + '/projects.html');
+    console.log("Projects");
+});
+
+website.get("/skills", function (req, res) {
+    res.sendFile(__dirname + '/skills.html');
+    console.log("Skills");
+});
+
+website.get("/work", function (req, res) {
+    res.sendFile(__dirname + '/work.html');
+    console.log("Work");
+});
+
+website.listen(80, function () {
     console.log("Server Success!");
 });
