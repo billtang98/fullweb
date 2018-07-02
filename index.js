ab9a5ab9cbc084a618213dfd0f0e0a86cf51b745
Yuwei Tang <yuweitang98@yahoo.ca>
2018-07-02 00:57:11 -0400

    console messages

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var website = express();
+var fs = require('fs');
+
+outputTime();
 
 website.use(express.static('assets'));
 
 website.get("/", function(req, res) {
-   res.sendFile(__dirname + '/index.html');
-   console.log("Home");
+    res.sendFile(__dirname + '/index.html');
+    console.log("Home");
+    visitedPage("Home");
 });
 
 website.get("/index", function (req, res) {
@@ -31,3 +35,33 @@ website.get("/work", function (req, res) {
 website.listen(5000, function () {
     console.log("Server Success!");
 });
+
+function visitedPage(data) {
+    data = data + "\n";
+    fs.appendFileSync('assets/visited.txt', data);
+}
+
+function outputTime() {
+    var date = new Date();
+    var m = date.getSeconds();
+    if (m == "0") {
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var amOrPm = "PM";
+        if (h < 12) {
+            amOrPm = "AM";
+        }
+        if (h == 0) {
+            h = 12;
+        }
+        if (h > 12) {
+            h = h - 12;
+        }
+        h = (h < 10) ? "0" + h : h;
+        m = (m < 10) ? "0" + m : m;
+
+        var time = h + ":00:00" + " " + amOrPm;
+        visitedPage(time);
+    }
+    setTimeout(outputTime, 60000);
+}
