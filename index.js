a520934995fc5c7b66f54370648d47f1a1828f71
Yuwei Tang <yuweitang98@yahoo.ca>
2018-07-02 01:58:09 -0400

    test2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var website = express();
 var fs = require('fs');
 
+startingServer();
 outputTime();
 
 website.use(express.static('assets'));
@@ -43,10 +44,9 @@ function visitedPage(data) {
 
 function outputTime() {
     var date = new Date();
-    var m = date.getSeconds();
+    var m = date.getMinutes();
     if (m == "0") {
         var h = date.getHours();
-        var m = date.getMinutes();
         var amOrPm = "PM";
         if (h < 12) {
             amOrPm = "AM";
@@ -58,10 +58,70 @@ function outputTime() {
             h = h - 12;
         }
         h = (h < 10) ? "0" + h : h;
-        m = (m < 10) ? "0" + m : m;
 
         var time = h + ":00:00" + " " + amOrPm;
         visitedPage(time);
     }
     setTimeout(outputTime, 60000);
+}
+
+function startingServer() {
+    var initialDate = new Date();
+    var date = initialDate.getDate();
+    var month = initialDate.getMonth();
+    var h = initialDate.getHours();
+    var m = initialDate.getMinutes();
+    var s = initialDate.getSeconds();
+    var amOrPm = "PM";
+    if (h < 12) {
+        amOrPm = "AM";
+    }
+    if (h == 0) {
+        h = 12;
+    }
+    if (h > 12) {
+        h = h - 12;
+    }
+    if (month == 0) {
+        month = "January";
+    }
+    else if (month == 1) {
+        month = "Feburary";
+    }
+    else if (month == 2) {
+        month = "March";
+    }
+    else if (month == 3) {
+        month = "April";
+    }
+    else if (month == 4) {
+        month = "May";
+    }
+    else if (month == 5) {
+        month = "June";
+    }
+    else if (month == 6) {
+        month = "July";
+    }
+    else if (month == 7) {
+        month = "August";
+    }
+    else if (month == 8) {
+        month = "September";
+    }
+    else if (month == 9) {
+        month = "October";
+    }
+    else if (month == 10) {
+        month = "November";
+    }
+    else {
+        month = "December";
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = month + " " + date + " " + h + ":" + m + ":" + s + " " + amOrPm;
+    visitedPage(time);
 }
